b610986a1939ca7faa6d2560bc63a9f9c27314c6
Brian <diazbrian47@gmail.com>
2023-09-19 11:48:40 -0500

    Agregado filtro para login

--- src/main/java/alura/foro/foroAPI/security/SecurityConfigurations.java
+++ src/main/java/alura/foro/foroAPI/security/SecurityConfigurations.java
@@ -1,5 +1,26 @@
 package alura.foro.foroAPI.security;
 
-public class SecurityConfigurations {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf
+						.disable())
+				.authorizeHttpRequests( authRequest -> authRequest
+				.requestMatchers("/auth/**").permitAll().anyRequest()
+				.authenticated())
+				.formLogin(withDefaults())
+				.build();
+	}
+	
 }
